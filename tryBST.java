c024704b4b4e1e29095a6d866de3b76a9389b772
MilkMan127 <4394348@myuwc.ac.za>
2026-03-23 12:38:21 +0000

    Added insert function for BST

--- tryBST.java
+++ tryBST.java
@@ -9,4 +9,19 @@ class tNode {
         left = right = null;
     }
 }
+public class tryBST {
+
+    tNode root;
+
+    
+    public tNode insert(tNode root, int key) {
+        if (root == null) return new tNode(key);
+
+        if (key < root.key)
+            root.left = insert(root.left, key);
+        else if (key > root.key)
+            root.right = insert(root.right, key);
+
+        return root;
+    }
 
